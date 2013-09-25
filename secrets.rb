cf6df9c26b6a9dccb7890d6e7d66d6f706a59380
MJ <michael.jones@dachisgroup.com>
2013-09-24 22:51:21 -0700

    oops, not so secret

--- secrets.rb
+++
@@ -1,8 +0,0 @@
-APP_NAME = 'Nameless APP'
-DEV_DB_NAME = 'somedb'
-BASE_URL = 'https://fancyfollowerfinder.herokuapp.com'
-TWITTER_BASE_URL = 'https://api.twitter.com'
-REGISTERED_REDIRECT_URI = BASE_URL + 'oauth2/callback'
-
-CONSUMER_KEY = 'gI8wsFqIGf8PVQHTRnukBg'
-CONSUMER_SECRET = 'dO2Dmq01ObPLHvZ8eyHgvab4kcB6VEP7fktN5HjC2xk'
